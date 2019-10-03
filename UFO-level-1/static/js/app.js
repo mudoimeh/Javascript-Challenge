a07993a5d9ee5385365ed8092c092e3d6f4160b9
mudoimeh <mudoimeh@gmail.com>
2019-10-02 23:07:38 -0500

    Add files via upload

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,46 @@
-// from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+//Select the tbody elements and input fields
+var tbody = d3.select("tbody");
+tableData.forEach(sighting => {
+   var row = tbody.append("tr");
+  Object.entries(sighting).forEach(([key, value]) => {
+       var cell = row.append("td");
+       cell.text(value);
+   });
+});
+
+// Add the button for the selection and select UFOdata
+var searchButton = d3.select("#filter-btn");
+searchButton.on("click", function() {
+   var inputElement = d3.select("#datetime");
+   var inputValue = inputElement.property("value");
+   console.log(inputValue);
+   console.log(tableData);
+
+   //Filter the data selected based on the date entered
+   
+   var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+   var sightings2 = filteredData.map(ufo => ufo);
+   tbody.html("");
+   if (inputValue) {
+   sightings2.forEach(ufosighting => {
+        var row2 = tbody.append("tr");
+      Object.entries(ufosighting).forEach(([key, value]) => {
+           var cell2 = row2.append("td");
+           cell2.text(value);
+       });
+   });
+}
+   else {
+       tbody.html("");
+   tableData.forEach(sighting => {
+   var row = tbody.append("tr");
+  Object.entries(sighting).forEach(([key, value]) => {
+       var cell = row.append("td");
+       cell.text(value);
+   });
+});
+   }
+});
